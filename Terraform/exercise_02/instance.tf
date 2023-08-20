3734756a6dd227237a1a5372df27ee8c3d443b07
Opeseen <Opeyemi5055@yahoo.com>
2023-08-20 20:47:39 +0100

    Deployed

--- Terraform/exercise_02/instance.tf
+++ Terraform/exercise_02/instance.tf
@@ -1,11 +1,11 @@
-resource "aws_instance" "amazonLinux" {
+resource "aws_instance" "ubuntuAmi" {
   ami                    = var.AMIS[var.REGION]
   instance_type          = "t2.micro"
-  availability_zone      = var.ZONE
-  key_name               = var.KeyPair[var.REGION]
-  vpc_security_group_ids = var.SG[var.REGION]
+  availability_zone      = var.ZONE1
+  key_name               = "dev01-key"
+  vpc_security_group_ids = ["sg-07bcc50a3f262a041"]
   tags = {
-    Name    = "amazonLinux-Instance"
-    Project = "Linux"
+    Name : "UbuntuIns"
+    Poject : "ubuntu"
   }
 }
--- Terraform/exercise_02/vars.tf
+++ Terraform/exercise_02/vars.tf
@@ -1,33 +1,15 @@
 variable "REGION" {
-  type        = string
-  default     = "us-east-1"
-  description = "VARIABLES_FILES"
+  default = "us-east-1"
 }
 
-variable "ZONE" {
+variable "ZONE1" {
   default = "us-east-1a"
 }
 
 variable "AMIS" {
   type = map(any)
   default = {
-    us-east-1 = "ami-0b5eea76982371e91"
-    us-east-2 = "ami-0ada6d94f396377f2"
-  }
-}
-
-variable "SG" {
-  type = map(any)
-  default = {
-    us-east-1 = ["sg-0c16777d5f4679998"]
-    us-east-2 = ["sg-0072935923ffe1330"]
-  }
-}
-
-variable "KeyPair" {
-  type = map(any)
-  default = {
-    us-east-1 = "web01-key"
-    us-east-2 = "web02-key"
+    us-east-1 = "ami-053b0d53c279acc90"
+    us-east-2 = "ami-024e6efaf93d85776"
   }
 }
