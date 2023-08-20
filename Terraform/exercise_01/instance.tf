929d2d49dd2181ad8f0329075f0b7f80b0284c59
Opeseen <Opeyemi5055@yahoo.com>
2023-08-20 17:58:56 +0100

    Deployed

--- Terraform/exercise_01/instance.tf
+++ Terraform/exercise_01/instance.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "intro" {
-  ami                    = "ami-0b5eea76982371e91"
+resource "aws_instance" "ubuntuInstance" {
+  ami                    = "ami-053b0d53c279acc90"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1a"
-  key_name               = "web01-key"
-  vpc_security_group_ids = ["sg-0c16777d5f4679998"]
+  key_name               = "dev01-key"
+  vpc_security_group_ids = ["sg-07bcc50a3f262a041"]
   tags = {
-    Name    = "CentOS-Instance"
-    Project = "Dove"
+    Name : "UbuntuIns"
+    Poject : "ubuntu"
   }
 }
