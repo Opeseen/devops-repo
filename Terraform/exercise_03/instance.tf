6d5c65cb756d5e5d77258085003c8108e8e2ca77
Opeseen <Opeyemi5055@yahoo.com>
2023-08-27 22:06:57 +0100

    Deployed

--- Terraform/exercise_03/instance.tf
+++ Terraform/exercise_03/instance.tf
@@ -1,35 +1,30 @@
-resource "aws_key_pair" "amazonLinux" {
-  key_name   = "web02-key"
+resource "aws_key_pair" "webKey" {
+  key_name   = "web02key"
   public_key = file("web02_key.pub")
 }
 
-resource "aws_instance" "amazonLinux" {
+resource "aws_instance" "tweenInstance" {
   ami                    = var.AMIS[var.REGION]
   instance_type          = "t2.micro"
-  availability_zone      = var.ZONE
-  key_name               = aws_key_pair.amazonLinux.key_name
-  vpc_security_group_ids = var.SG[var.REGION]
+  availability_zone      = var.ZONE1
+  key_name               = aws_key_pair.webKey.key_name
+  vpc_security_group_ids = ["sg-07bcc50a3f262a041"]
   tags = {
-    Name    = "amazonLinux-Instance"
-    Project = "Linux"
+    Name : "TweenIns"
   }
-
   provisioner "file" {
     source      = "web.sh"
     destination = "/tmp/web.sh"
   }
-
   provisioner "remote-exec" {
     inline = [
       "chmod u+x /tmp/web.sh",
       "sudo /tmp/web.sh"
     ]
   }
-
   connection {
     user        = var.USER
     private_key = file("web02_key")
     host        = self.public_ip
   }
-
 }
--- Terraform/exercise_03/vars.tf
+++ Terraform/exercise_03/vars.tf
@@ -1,25 +1,16 @@
 variable "REGION" {
-  type    = string
   default = "us-east-1"
 }
 
-variable "ZONE" {
+variable "ZONE1" {
   default = "us-east-1a"
 }
 
 variable "AMIS" {
   type = map(any)
   default = {
-    us-east-1 = "ami-0b5eea76982371e91"
-    us-east-2 = "ami-0ada6d94f396377f2"
-  }
-}
-
-variable "SG" {
-  type = map(any)
-  default = {
-    us-east-1 = ["sg-0c16777d5f4679998"]
-    us-east-2 = ["sg-0072935923ffe1330"]
+    us-east-1 = "ami-051f7e7f6c2f40dc1"
+    us-east-2 = "ami-0cf0e376c672104d6"
   }
 }
 
