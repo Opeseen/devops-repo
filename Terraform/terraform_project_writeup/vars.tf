3d8d3282b579a7c20b65904510749b5eb68f1319
Opeseen <Opeyemi5055@yahoo.com>
2024-01-06 10:13:36 +0100

    Deployed

--- Terraform/terraform_project_writeup/vars.tf
+++ Terraform/terraform_project_writeup/vars.tf
@@ -28,18 +28,18 @@ variable "VPC_NAME" {
 }
 
 variable "VPC_CIDR" {
-  default = "172.21.0.0/16"
+  default = "10.0.0.0/16"
 }
 
 variable "PUB_SUB1_CIDR" {
-  default = "172.21.1.0/24"
+  default = "10.0.1.0/24"
 }
 
 variable "PUB_SUB2_CIDR" {
-  default = "172.21.2.0/24"
+  default = "10.0.2.0/24"
 }
 
 variable "PUB_SUB3_CIDR" {
-  default = "172.21.3.0/24"
+  default = "10.0.3.0/24"
 }
 
