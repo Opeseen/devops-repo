57efc7cf950c2777b3a5a9d8970a36499ff161ea
Opeseen <Opeyemi5055@yahoo.com>
2024-03-02 21:01:54 +0100

    Deployed

--- Terraform/terraform_project_writeup/bean-app.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_elastic_beanstalk_application" "banking-beanstalk-app" {
-  name = "bank-beanstalk-app"
-}
--- Terraform/terraform_project_writeup/keypair.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "bank_app_key" {
-  key_name   = "bank-key"
-  public_key = file(var.PUB_KEY)
-}
